142c69952351163ec79e58b1ab7cb7a8cd34b868
heebum kim <heebum9955@naver.com>
2025-05-08 16:19:35 +0900

    chore: maven 프로젝트로 구조 변경

---
+++ src/main/java/com/gobookee/common/JDBCTemplate.java
@@ -0,0 +1,78 @@
+package com.gobookee.common;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCTemplate {
+    private static Properties driver = new Properties();
+
+    static {
+        String path = JDBCTemplate.class.getResource("/config/db.properties").getPath();
+        try (FileReader fr = new FileReader(path)) {
+            driver.load(fr);
+            Class.forName(driver.getProperty("driver"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    driver.getProperty("url"),
+                    driver.getProperty("user"),
+                    driver.getProperty("pw")
+            );
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void close(Connection obj) {
+        try {
+            if (obj != null && !obj.isClosed()) obj.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Statement obj) {
+        try {
+            if (obj != null && !obj.isClosed()) obj.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet obj) {
+        try {
+            if (obj != null && !obj.isClosed()) obj.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void commit(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rollback(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) conn.rollback();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head><title>Hello JSP</title></head>
+<body>
+<h2>Hello, GoBookEE!</h2>
+<%= "현재 시간: " + new java.util.Date() %>
+</body>
+</html>
