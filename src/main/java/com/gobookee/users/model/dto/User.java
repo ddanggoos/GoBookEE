bd8c74e5f5eb3d5b1ab8661cd3b2f093768e7307
heebum kim <heebum9955@naver.com>
2025-05-10 16:58:43 +0900

    style: 코드 줄바꿈 변경

--- src/main/java/com/gobookee/users/model/dto/User.java
+++ src/main/java/com/gobookee/users/model/dto/User.java
@@ -10,19 +10,30 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 public class User {
     private Long UserSeq;
+
     private String UserId;
+
     private String UserPwd;
+
     private String UserName;
+
     //Gender는 MALE, FEMALE로 구분
     private Gender UserGender;
+
     private String UserPhone;
+
     //프로필 사진은 PresignedUrl 방식을 사용해서 url 주소만 DB에 저장
     private String UserProfile;
+
     private String UserIntro;
+
     //UserType은 USER, ADMIN, OWNER로 구분
     private UserType UserType;
+
     private String UserEmail;
+
     private Timestamp UserCreateTime;
+
     private Timestamp UserDeleteTime;
 }
 
