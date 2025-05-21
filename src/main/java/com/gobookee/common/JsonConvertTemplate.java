d7105f11a31629a8c5bfb5e2f5be0fcdeb1a0cfe
heebum kim <heebum9955@naver.com>
2025-05-21 19:49:34 +0900

    feat: JsonConvertTemplate 구현 Gson 라이브러리 사용해서 JSON 데이터 컨버팅 템플릿 구현

---
+++ src/main/java/com/gobookee/common/JsonConvertTemplate.java
@@ -0,0 +1,33 @@
+package com.gobookee.common;
+
+import com.google.gson.Gson;
+
+/**
+ * Gson 라이브러리로 JSON <-> 객체 변환 템플릿
+ */
+public class JsonConvertTemplate {
+    private static final Gson gson = new Gson();
+
+    /**
+     * JSON 문자열을 자바 객체로 변환해주는 메소드
+     * 문자열, 클래스 타입 인자로 전달해주면 해당 타입 객체 반환
+     *
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T formJson(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+
+    /**
+     * 자바 객체를 JSON 문자열로 변환해주는 메소드
+     * 자바 객체를 인자로 전달해주면 JSON 문자열 반환
+     * @param obj
+     * @return
+     */
+    public static String toJson(Object obj) {
+        return gson.toJson(obj);
+    }
+}
