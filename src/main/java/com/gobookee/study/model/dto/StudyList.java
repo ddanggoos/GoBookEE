e26b7a68fb416e489cb7cb0d2b18427ac620c75b
heebum kim <heebum9955@naver.com>
2025-05-25 21:08:09 +0900

    refactor: 기본타입 -> 래퍼타입으로 수정

--- src/main/java/com/gobookee/study/model/dto/StudyList.java
+++ src/main/java/com/gobookee/study/model/dto/StudyList.java
@@ -1,24 +1,24 @@
 package com.gobookee.study.model.dto;
 
-import java.sql.Timestamp;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
 public class StudyList {
-	private long studySeq;
-	private String studyTitle;
-	private Timestamp studyDate;
-	private int studyMemberLimit;
-	private String studyPlace;
-	private int confirmedCount;
-	private String photoRenamedName;
-	private int likeCount;
-	private int dislikeCount;
+    private Long studySeq;
+    private String studyTitle;
+    private Timestamp studyDate;
+    private Integer studyMemberLimit;
+    private String studyPlace;
+    private Integer confirmedCount;
+    private String photoRenamedName;
+    private Integer likeCount;
+    private Integer dislikeCount;
 }
