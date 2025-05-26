d43627a9199c77f2744c1bdd0cddb380fa732cbb
ddanggoos <hanck9595@gmail.com>
2025-05-26 11:47:12 +0900

    feat : 책 상세 서블릿 추가

---
+++ src/main/java/com/gobookee/book/controller/BookDetailServlet.java
@@ -0,0 +1,28 @@
+package com.gobookee.book.controller;
+
+import com.gobookee.book.model.dto.Book;
+import com.gobookee.book.service.BookService;
+import com.gobookee.common.CommonPathTemplate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/books/bookdetail")
+public class BookDetailServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int bookSeq = Integer.parseInt(request.getParameter("bookSeq"));
+        Book book = BookService.bookService().getBookDetailBySeq(bookSeq);
+        request.setAttribute("book", book);
+        request.getRequestDispatcher(CommonPathTemplate.getViewPath("/book/bookDetail")).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
