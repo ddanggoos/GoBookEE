0ddb007c6edf2670bf4ba46d99982020dfde281a
sysypp <parkseoyoung0915@gmail.com>
2025-05-27 23:19:16 +0900

    feat: 추천 비추천 기능 구현

--- src/main/java/com/gobookee/recommend/controller/NonRecInsertServlet.java
+++ src/main/java/com/gobookee/recommend/controller/NonRecInsertServlet.java
@@ -1,41 +1,51 @@
 package com.gobookee.recommend.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class NonRecInsertServlet
- */
+import com.gobookee.recommend.service.RecommendService;
+import com.gobookee.users.model.dto.User;
+import com.google.gson.Gson;
+
 @WebServlet("/nonrecommend/insert")
 public class NonRecInsertServlet extends HttpServlet {
+	private RecommendService service = RecommendService.recommendService();
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NonRecInsertServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public NonRecInsertServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Long boardSeq = Long.parseLong(request.getParameter("boardSeq"));
+		User loginUser = (User) request.getSession().getAttribute("loginUser");
+		Long userSeq = loginUser.getUserSeq();
+
+		Gson gson = new Gson();
+		response.setContentType("application/json;charset=UTF-8");
+		Map<String, Object> result = new HashMap<>();
+		try {
+			result = service.toggleNonRecommend(userSeq, boardSeq);
+			result.put("success", true);
+		} catch (Exception e) {
+			result.put("success", false);
+			result.put("message", "처리 중 오류 발생");
+			e.printStackTrace();
+		}
+
+		gson.toJson(result, response.getWriter());
 	}
 
 }
---
+++ src/main/java/com/gobookee/recommend/service/RecommendService.java
@@ -0,0 +1,79 @@
+package com.gobookee.recommend.service;
+
+import static com.gobookee.common.JDBCTemplate.close;
+import static com.gobookee.common.JDBCTemplate.commit;
+import static com.gobookee.common.JDBCTemplate.getConnection;
+import static com.gobookee.common.JDBCTemplate.rollback;
+import static com.gobookee.recommend.model.dao.RecommendDAO.recommendDao;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.gobookee.recommend.model.dao.RecommendDAO;
+import com.gobookee.review.model.dto.Review;
+
+public class RecommendService {
+
+	private static final RecommendService SERVICE = new RecommendService();
+
+	private RecommendDAO dao = recommendDao();
+
+	private RecommendService() {
+	}
+
+	public static RecommendService recommendService() {
+		return SERVICE;
+	}
+
+	public Map<String, Object> toggleRecommend(Long userSeq, Long boardSeq) {
+		Connection conn = getConnection();
+		Map<String, Object> result = new HashMap<>();
+
+		try {
+			if (dao.exists(conn, userSeq, boardSeq, "0")) {// 추천이 있는상태면
+				dao.delete(conn, userSeq, boardSeq); // 추천 취소
+			} else {								 //추천이 없는 상태여도
+				dao.delete(conn, userSeq, boardSeq); // 비추천도 있을 수 있으니 삭제
+				dao.insert(conn, userSeq, boardSeq, "0"); // 추천등록
+			}
+
+			result.put("recommendCount", dao.countByType(conn, boardSeq, "0"));
+			result.put("nonRecommendCount", dao.countByType(conn, boardSeq, "1"));
+
+			commit(conn);
+		} catch (Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+		} finally {
+			close(conn);
+		}
+
+		return result;
+	}
+
+	public Map<String, Object> toggleNonRecommend(Long userSeq, Long boardSeq) {
+		Connection conn = getConnection();
+		Map<String, Object> result = new HashMap<>();
+		try {
+			if (dao.exists(conn, userSeq, boardSeq, "1")) {
+				dao.delete(conn, userSeq, boardSeq);
+			} else {
+				dao.delete(conn, userSeq, boardSeq);
+				dao.insert(conn, userSeq, boardSeq, "1");
+			}
+
+			result.put("recommendCount", dao.countByType(conn, boardSeq, "0"));
+			result.put("nonRecommendCount", dao.countByType(conn, boardSeq, "1"));
+
+			commit(conn);
+		} catch (Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+		} finally {
+			close(conn);
+		}
+
+		return result;
+	}
+}
