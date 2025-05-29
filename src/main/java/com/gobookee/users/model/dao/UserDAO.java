b3c711b1ce5ecef20b389f662f25c5de60c14942
heebum kim <heebum9955@naver.com>
2025-05-29 09:06:42 +0900

    refactor: 유저 거북이 속도 수정 기능 리팩토링

--- src/main/java/com/gobookee/users/model/dao/UserDAO.java
+++ src/main/java/com/gobookee/users/model/dao/UserDAO.java
@@ -1,5 +1,10 @@
 package com.gobookee.users.model.dao;
 
+import com.gobookee.common.JDBCTemplate;
+import com.gobookee.users.model.dto.Gender;
+import com.gobookee.users.model.dto.User;
+import com.gobookee.users.model.dto.UserType;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
@@ -8,125 +13,142 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import com.gobookee.common.JDBCTemplate;
-import com.gobookee.users.model.dto.Gender;
-import com.gobookee.users.model.dto.User;
-import com.gobookee.users.model.dto.UserType;
-
 
 public class UserDAO {
-	private Properties sqlProp=new Properties();
-	private PreparedStatement pstmt;
-	private ResultSet rs;
-	
-	private static final UserDAO userDao=new UserDAO();
-	
-	private UserDAO() {
-		String path=UserDAO.class.getResource("/config/users-sql.properties").getPath();
-		try(FileReader fr=new FileReader(path)) {
-			sqlProp.load(fr);
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public static UserDAO userDao() {
-		return userDao;
-	}
-	
-	public int insertUser(Connection conn, User u) {
-	    int result = 0;
-	    PreparedStatement pstmt = null;
+    private Properties sqlProp = new Properties();
+    private PreparedStatement pstmt;
+    private ResultSet rs;
 
-	    try {
-	        String sql = sqlProp.getProperty("insertUser");
-	        pstmt = conn.prepareStatement(sql);
-	        
-	        pstmt.setString(1, u.getUserId());
-	        pstmt.setString(2, u.getUserPwd());
-	        pstmt.setString(3, u.getUserNickName());
-	        pstmt.setString(4, u.getUserGender().toString());
-	        pstmt.setString(5, u.getUserPhone());
-	        pstmt.setString(6, u.getUserAddress()); 
-	        pstmt.setString(7, u.getUserProfile());
-	        pstmt.setString(8, u.getUserIntro()); 
-	        pstmt.setString(9, u.getUserType().toString());	        
-	        pstmt.setString(10, u.getUserEmail());
-	        result = pstmt.executeUpdate();
-	        System.out.println(result);
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    } finally {
-	        JDBCTemplate.close(pstmt);
-	    }
+    private static final UserDAO userDao = new UserDAO();
 
-	    return result;
-	}
-	
-	public User searchUserById(Connection conn, String userId) {
-		User u=null;
-		try {
-			pstmt=conn.prepareStatement(sqlProp.getProperty("searchById"));
-			pstmt.setString(1, userId);
-			rs=pstmt.executeQuery();
-			if(rs.next()) u=getUser(rs);
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JDBCTemplate.close(rs);
-			JDBCTemplate.close(pstmt);
-		}return u;
-	}
-	
-	public User searchUserByNickName(Connection conn, String userNickname) {
-		User u = null;
-		try {
-			pstmt = conn.prepareStatement(sqlProp.getProperty("searchByNickName"));
-			pstmt.setString(1, userNickname);
-			rs=pstmt.executeQuery();
-			if(rs.next()) u=getUser(rs);
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}finally {
-			JDBCTemplate.close(rs);
-			JDBCTemplate.close(pstmt);
-		}return u;
-	}
-	
-	//resultset의 결과를 유저DTO로 변환해주는 기능 
-	private User getUser(ResultSet rs) throws SQLException {
-	    return User.builder()
-	            .UserSeq(rs.getLong("user_seq"))
-	            .UserId(rs.getString("user_id"))
-	            .UserPwd(rs.getString("user_pwd"))
-	            .UserNickName(rs.getString("user_nickname"))
-	            .UserGender(parseGender(rs.getString("user_gender")))
-	            .UserPhone(rs.getString("user_phone"))
-	            .UserProfile(rs.getString("user_profile"))
-	            .UserIntro(rs.getString("user_intro"))
-	            .UserType(parseUserType(rs.getString("user_type")))
-	            .UserEmail(rs.getString("user_email"))
-	            .UserCreateTime(rs.getTimestamp("user_create_time"))
-	            .UserDeleteTime(rs.getTimestamp("user_delete_time"))
-	            .build();
-	}
-	
-	private Gender parseGender(String value) {
-	    if (value == null) return null;
-	    switch (value.toUpperCase()) {
-	        case "M": return Gender.M;
-	        case "F": return Gender.F;
-	        default: throw new IllegalArgumentException("GENDER: " + value);
-	    }
-	}
-	
-	private UserType parseUserType(String value) {
-	    if (value == null) return null;
-	    switch (value) {
-	        case "0": return UserType.USER;
-	        case "1": return UserType.OWNER;
-	        default: throw new IllegalArgumentException("TYPE : " + value);
-	    }
-	}
+    private UserDAO() {
+        String path = UserDAO.class.getResource("/config/users-sql.properties").getPath();
+        try (FileReader fr = new FileReader(path)) {
+            sqlProp.load(fr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	
+    public static UserDAO userDao() {
+        return userDao;
+    }
+
+    public int insertUser(Connection conn, User u) {
+        int result = 0;
+        PreparedStatement pstmt = null;
+
+        try {
+            String sql = sqlProp.getProperty("insertUser");
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, u.getUserId());
+            pstmt.setString(2, u.getUserPwd());
+            pstmt.setString(3, u.getUserNickName());
+            pstmt.setString(4, u.getUserGender().toString());
+            pstmt.setString(5, u.getUserPhone());
+            pstmt.setString(6, u.getUserAddress());
+            pstmt.setString(7, u.getUserProfile());
+            pstmt.setString(8, u.getUserIntro());
+            pstmt.setString(9, u.getUserType().toString());
+            pstmt.setString(10, u.getUserEmail());
+            result = pstmt.executeUpdate();
+            System.out.println(result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCTemplate.close(pstmt);
+        }
+
+        return result;
+    }
+
+    public User searchUserById(Connection conn, String userId) {
+        User u = null;
+        try {
+            pstmt = conn.prepareStatement(sqlProp.getProperty("searchById"));
+            pstmt.setString(1, userId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) u = getUser(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCTemplate.close(rs);
+            JDBCTemplate.close(pstmt);
+        }
+        return u;
+    }
+
+    public User searchUserByNickName(Connection conn, String userNickname) {
+        User u = null;
+        try {
+            pstmt = conn.prepareStatement(sqlProp.getProperty("searchByNickName"));
+            pstmt.setString(1, userNickname);
+            rs = pstmt.executeQuery();
+            if (rs.next()) u = getUser(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCTemplate.close(rs);
+            JDBCTemplate.close(pstmt);
+        }
+        return u;
+    }
+
+    public void updateUserSpeed(Connection conn, Long userSeq, int change) {
+        try {
+            pstmt = conn.prepareStatement(sqlProp.getProperty("updateUserSpeed"));
+            pstmt.setInt(1, change);
+            pstmt.setLong(2, userSeq);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCTemplate.close(pstmt);
+        }
+    }
+
+    //resultset의 결과를 유저DTO로 변환해주는 기능
+    private User getUser(ResultSet rs) throws SQLException {
+        return User.builder()
+                .UserSeq(rs.getLong("user_seq"))
+                .UserId(rs.getString("user_id"))
+                .UserPwd(rs.getString("user_pwd"))
+                .UserNickName(rs.getString("user_nickname"))
+                .UserGender(parseGender(rs.getString("user_gender")))
+                .UserPhone(rs.getString("user_phone"))
+                .UserProfile(rs.getString("user_profile"))
+                .UserIntro(rs.getString("user_intro"))
+                .UserType(parseUserType(rs.getString("user_type")))
+                .UserEmail(rs.getString("user_email"))
+                .UserCreateTime(rs.getTimestamp("user_create_time"))
+                .UserDeleteTime(rs.getTimestamp("user_delete_time"))
+                .build();
+    }
+
+    private Gender parseGender(String value) {
+        if (value == null) return null;
+        switch (value.toUpperCase()) {
+            case "M":
+                return Gender.M;
+            case "F":
+                return Gender.F;
+            default:
+                throw new IllegalArgumentException("GENDER: " + value);
+        }
+    }
+
+    private UserType parseUserType(String value) {
+        if (value == null) return null;
+        switch (value) {
+            case "0":
+                return UserType.USER;
+            case "1":
+                return UserType.OWNER;
+            default:
+                throw new IllegalArgumentException("TYPE : " + value);
+        }
+    }
+
+
 }
