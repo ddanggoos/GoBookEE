dc3519928b6c20514c02bf1b7f269fbe6eabddeb
RoastedBear <kinim329@gmail.com>
2025-05-31 22:34:16 +0900

    feat/스터디페이지 프론트 구현

--- src/main/java/com/gobookee/studygroup/controller/StudyGroupListServlet.java
+++ src/main/java/com/gobookee/studygroup/controller/StudyGroupListServlet.java
@@ -70,6 +70,7 @@ public class StudyGroupListServlet extends HttpServlet {
 		result.put("pageBar", pb.makePageBar(request));
 		
 		gson.toJson(result, response.getWriter());
+		System.out.println(userseq+"/"+list);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
